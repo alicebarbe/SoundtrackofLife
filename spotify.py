13e0b3db82d53707c54e6a8421e0f728054d98c7
Alice Barbe <alice_barbe@hotmail.com>
2022-02-20 01:32:44 -0500

    hacky mostly non-repeating playback loop

--- spotify.py
+++ spotify.py
@@ -17,7 +17,7 @@ import pandas as pd
 with open('credentials.json', 'r') as f:
     cred = json.load(f)
 
-scope = "user-library-read-playback"
+scope = "user-library-read"
 
 ruri = r"https://www.google.com"
 
@@ -58,11 +58,15 @@ def get_playlist_df(token):
             print(f"{i}: {track_name} - {track_artist}")
             i += 1
     else:
-        print("Can't get token for", username)
+        print("Can't get token for", cred['spotify']['username'])
 
     return playlist_df
 
 playlist_df = get_playlist_df(token)
+# %%
+playlist_df.to_pickle("playlist_df1.pkl")
+# %%
+playlist_df = pd.read_pickle("playlist_df.pkl")
 
 # %% generate tree
 from scipy import spatial
@@ -71,19 +75,60 @@ k=1 # gets incremented to prevent same song from being played over and over agai
 tree = spatial.KDTree(valence_energy_pairs)
 
 # %% PLAY MUSIC
-
-_, closest_index = tree.query([0, 0.001], k=[k])
-closest_index = closest_index[0]
-print(playlist_df.loc[closest_index, :])
-
-scope = "streaming"
+scope = "streaming user-read-playback-state"
 token = spotipy.util.prompt_for_user_token(cred['spotify_premium']['username'], scope,
                                            client_id=cred['spotify_premium']['client_id'],
                                            client_secret=cred['spotify_premium']['client_secret'],
                                            redirect_uri=ruri)
 sp = spotipy.Spotify(auth=token)
-sp.start_playback(device_id=cred['spotify_premium']['device_id'],
-                  uris=[playlist_df.loc[closest_index, 'uri']])
+device_id = sp.devices()['devices'][0]['id']
+
+def get_song(valence_energy_coord, k=1):
+    _, closest_index = tree.query(valence_energy_coord, k=[k])
+    closest_index = closest_index[0]
+    print(playlist_df.loc[closest_index, :])
+
+    current_uri = playlist_df.loc[closest_index, 'uri']
+    return current_uri
+
+ki = 1
+i = 0
+import time
+uri_list = []
+current_uri = get_song([0.2, 0.2])
+uri_list.append(current_uri)
+
+sp.start_playback(device_id=device_id, uris=[current_uri])
+
+while i<100:
+    # if song is still playing
+    if sp.current_playback()['is_playing']:
+        time.sleep(i)
+        i += 1
+        print(i)
+        pass
+    else:
+        print("song stopped")
+        coord = json.loads(input("Enter coords:"))
+        print(coord)
+
+        while ki < 10:
+            if get_song(coord, k=1) not in uri_list:
+                ki = 1
+                break
+            else:
+                ki += 1
+                if get_song(coord, k=ki) not in uri_list:
+                    break
+        else:
+            print("giving up, setting k to 1")
+            ki = 1
+
+        print(f"ki: {ki}")
+        current_uri = get_song(coord, ki)
+        uri_list.append(current_uri)
+        sp.start_playback(device_id=device_id, uris=[current_uri])
+
 
 # %% plot values of library
 import plotly.graph_objects as go
@@ -92,5 +137,6 @@ import plotly.express as px
 
 fig = px.scatter(playlist_df, x="valence", y="energy",
                  hover_data=['name'])
+
 
 htmlplot(fig)
